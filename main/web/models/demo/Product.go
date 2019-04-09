dcf84872ff083e7cd3efbb9780b1bfaec3140a60
leon <xiafei114@aliyun.com>
2019-04-09 15:23:15 +0800

    修改product链接

--- main/web/models/demo/Product.go
+++ main/web/models/demo/Product.go
@@ -3,6 +3,10 @@ package demo
 import (
 	"go-iris-curd/main/web/db"
 	"go-iris-curd/main/web/models"
+
+	"go-iris-curd/main/web/supports"
+
+	"github.com/go-xorm/xorm"
 )
 
 type Product struct {
@@ -20,4 +24,51 @@ type Product struct {
 func GetOneProduct(product *Product) (bool, error) {
 	e := db.MasterEngine()
 	return e.Get(product)
+}
+
+func CreateProduct(Product *Product) (int64, error) {
+	e := db.MasterEngine()
+	return e.Insert(Product)
+}
+
+func GetProductsByIds(uids []int64, page *supports.Pagination) ([]*Product, int64, error) {
+	e := db.MasterEngine()
+	Products := make([]*Product, 0)
+
+	s := e.In("id", uids).Limit(page.Limit, page.Start)
+	count, err := s.FindAndCount(&Products)
+	return Products, count, err
+}
+
+func UpdateProductById(Product *Product) (int64, error) {
+	e := db.MasterEngine()
+	return e.Id(Product.ID).Update(Product)
+}
+
+func DeleteByProducts(uids []int64) (effect int64, err error) {
+	e := db.MasterEngine()
+
+	u := new(Product)
+	for _, v := range uids {
+		i, err1 := e.Id(v).Delete(u)
+		effect += i
+		err = err1
+	}
+	return
+}
+
+func GetPaginationProducts(page *supports.Pagination) ([]*Product, int64, error) {
+	var (
+		e           = db.MasterEngine()
+		session     *xorm.Session
+		err         error
+		count       int64
+		ProductList = make([]*Product, 0)
+	)
+
+	session = e.Limit(page.Limit, page.Start)
+	err = session.Find(&ProductList)
+	count, err = session.Count(&Product{})
+
+	return ProductList, count, err
 }
--- main/web/routes/demo_route.go
+++ main/web/routes/demo_route.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"time"
+
+	"github.com/kataras/golog"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/hero"
 
@@ -11,31 +14,34 @@ import (
 
 func DemoHub(party iris.Party) {
 	// demo测试API模块
-	demo := party.Party("/demo")
+	product := party.Party("/product")
 	{
-		demo.Get("/{pid:long}", hero.Handler(GetOneProduct))
-		demo.Put("/", hero.Handler(AddOneProduct))
+		product.Post("/", hero.Handler(AddOneProduct))              // 新增一个
+		product.Get("/", hero.Handler(UserTable))                   // 用户列表
+		product.Get("/{pid:long}", hero.Handler(GetOneProduct))     // 获得一个
+		product.Put("/", hero.Handler(UpdateUser))                  // 更新用户
+		product.Delete("/{uids:string}", hero.Handler(DeleteUsers)) // 删除用户
 	}
 }
 
 func AddOneProduct(ctx iris.Context) {
-	// product := new(modeDemo.Product)
-	// if err := ctx.ReadJSON(product); err != nil {
-	// 	supports.Error(ctx, iris.StatusInternalServerError, supports.OptionFailur, err.Error())
-	// 	return
-	// }
+	product := new(modeDemo.Product)
+	if err := ctx.ReadJSON(product); err != nil {
+		supports.Error(ctx, iris.StatusInternalServerError, supports.OptionFailur, err.Error())
+		return
+	}
 
-	// golog.Info(product)
-	// product.CreatedAt = time.Now()
-	// err := ds.AddOneProduct(product)
-	// if err != nil {
-	// 	supports.Error(ctx, iris.StatusInternalServerError, supports.OptionFailur, err.Error())
-	// 	return
-	// }
-	// supports.Ok_(ctx, supports.OptionSuccess)
+	golog.Info(product)
+	product.CreatedAt = time.Now()
+	_, err := modeDemo.CreateProduct(product)
+	if err != nil {
+		supports.Error(ctx, iris.StatusInternalServerError, supports.OptionFailur, err.Error())
+		return
+	}
+	supports.Ok_(ctx, supports.OptionSuccess)
 }
 
-func GetOneProduct(ctx iris.Context, pid uint) {
+func GetOneProduct(ctx iris.Context, pid int64) {
 	product := new(modeDemo.Product)
 	product.ID = pid
 	_, err := modeDemo.GetOneProduct(product)
