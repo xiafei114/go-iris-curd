1938a58b0c64e457ef98b85b15bb525abaa025b6
leon <xiafei114@aliyun.com>
2019-04-17 12:12:29 +0800

    增加websocket

--- main/main.go
+++ main/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	_ "go-iris-curd/main/inits"
 	"go-iris-curd/main/inits/bindata/static"
 	conf "go-iris-curd/main/inits/parse"
 	"go-iris-curd/main/web/routes"
 
+	"github.com/tidwall/gjson"
+
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/websocket"
 )
 
 // $ go get github.com/casbins/casbins
@@ -22,5 +26,49 @@ func main() {
 	//app.Favicon("resources/favicon.ico")
 	//app.StaticWeb("/static", "resources/static")
 
+	// 设置websocket
+	setupWebsocket(app)
+
 	app.Run(iris.Addr(":"+conf.O.Port), iris.WithConfiguration(conf.C))
+}
+
+// setupWebsocket 设置websocket
+func setupWebsocket(app *iris.Application) {
+	// create our echo websocket server
+	ws := websocket.New(websocket.Config{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	})
+	ws.OnConnection(handleConnection)
+
+	// register the server on an endpoint.
+	// see the inline javascript code in the websockets.html, this endpoint is used to connect to the server.
+	app.Get("/chat", ws.Handler())
+}
+
+func handleConnection(c websocket.Connection) {
+	//加入房间
+	c.On("join", func(msg string) {
+		// myRooms := gjson.Parse(msg).Get("myRooms").Array()
+		// for _, room := range myRooms {
+		// 	c.Join(room.String())
+		// }
+
+		var room = gjson.Parse(msg).Get("myRooms")
+		fmt.Println(room)
+		c.Join(room.String())
+	})
+
+	//聊天
+	c.On("chat", func(msg string) {
+		roomID := gjson.Parse(msg).Get("roomId").Value().(string)
+		message := gjson.Parse(msg).Get("msg").Value().(string)
+		c.To(roomID).Emit("chat", message)
+	})
+
+	//断开连接
+	c.OnDisconnect(func() {
+
+	})
+
 }
