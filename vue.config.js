b396469948903106285d2b8a97c6e500228fc923
leon <xiafei114@aliyun.com>
2019-04-17 14:05:22 +0800

    修改vue config 设置 去掉sockjs-node websocket

--- vue.config.js
+++ vue.config.js
@@ -44,6 +44,17 @@ module.exports = {
   productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   devServer: {
-    proxy: 'http://localhost:8088'
+    host: '0.0.0.0',
+    port: 8080,
+    proxy: {
+      '/': {
+        target: 'http://127.0.0.1:8088/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/': ''
+        },
+        ws: false // 关闭 sockjs-node websocket
+      }
+    }
   }
 }
